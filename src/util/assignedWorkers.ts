654c7d91a3025d1982c4bfcdf25485883b5388e4
anonymousrecords <a.Records2022@gmail.com>
2024-04-27 18:11:54 +0900

    feat : 담당자 할당 함수 구현

---
+++ src/util/assignedWorkers.ts
@@ -0,0 +1,18 @@
+// Info: 주말 배열에 작업자 할당하기
+import { AssignedWorkers, personList, WeekendArray } from "@/data/type";
+
+export const assignWorkers = (weekendArray: WeekendArray[]): AssignedWorkers[] => {
+  const workers = [];
+  let luckyPersonId = 0;
+
+  // 주말에 대해 반복
+  for (let i = 0; i < weekendArray.length; i++) {
+    const date = weekendArray[i].date;
+    const worker1 = personList[luckyPersonId % personList.length].name;
+    luckyPersonId++;
+    const worker2 = personList[luckyPersonId % personList.length].name;
+    luckyPersonId++;
+    workers.push({ date, workers: [worker1, worker2] });
+  }
+  return workers;
+};
