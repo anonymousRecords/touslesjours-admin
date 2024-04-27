b3efefa422e3cb8d784b6a54292a55d1b1645a67
anonymousrecords <a.Records2022@gmail.com>
2024-04-27 18:12:15 +0900

    feat : 작업 횟수 카운트 함수 구현

---
+++ src/util/workCounter.ts
@@ -0,0 +1,17 @@
+import { AssignedWorkers, WeekendArray } from '@/data/type';
+
+// 주말 배열에서 특정 담당자의 언급 횟수를 카운트하는 함수
+const mentionCounter = (personName: string, assignedWorkers: AssignedWorkers[]): number => {
+  let mentionCount = 0;
+
+  // 주말 배열을 순회하면서 특정 담당자의 언급 횟수를 카운트
+  for (const assingedWorkds of assignedWorkers) {
+    const workers = assingedWorkds.workers;
+    // workers 배열에서 "일번사람"이 언급된 횟수를 카운트
+    mentionCount += workers.filter(worker => worker === personName).length;
+  }
+
+  return mentionCount;
+};
+
+export default mentionCounter;
