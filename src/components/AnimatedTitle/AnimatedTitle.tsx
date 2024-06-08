26c8eb4cdd2808606ed345c3b81f11ab7e49a569
anonymousrecords <a.Records2022@gmail.com>
2024-06-09 01:32:32 +0900

    feat : 홈 페이지 구현

---
+++ src/components/AnimatedTitle/AnimatedTitle.tsx
@@ -0,0 +1,11 @@
+const AnimatedTitle = ({ direction }) => (
+  <div className="animated-title">
+    <div className={`track track-${direction}`}>
+      <div className="content font-['April16th-Promise']">
+        &nbsp;뚜레쥬르&nbsp;뚜레쥬르&nbsp;뚜레쥬르&nbsp;뚜레쥬르&nbsp;뚜레쥬르&nbsp;뚜레쥬르
+      </div>
+    </div>
+  </div>
+);
+
+export default AnimatedTitle;
