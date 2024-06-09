6b7c1a826fa13d8ef9001a8f52fdc5cb671bc056
anonymousrecords <a.Records2022@gmail.com>
2024-06-09 18:33:22 +0900

    feat : weekdayFromNumber 추가

---
+++ src/util/weekdayFromNumber.ts
@@ -0,0 +1,17 @@
+import { sandwichRows } from "@/data/type";
+
+export const weekdayFromNumber = (numberOrString) => {
+    if (typeof numberOrString === 'number') {
+      const index = (numberOrString) % 7;
+      return sandwichRows[index];
+    } else if (typeof numberOrString === 'string') {
+      const index = sandwichRows.indexOf(numberOrString);
+      if (index !== -1) {
+        return index + 1;
+      } else {
+        return "weekdayFromNumber Error: Invalid input";
+      }
+    } else {
+      return "weekdayFromNumber Error: Invalid input";
+    }
+  };
