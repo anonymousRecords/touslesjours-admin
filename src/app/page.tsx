b8fa71398bf178763b5014cd36122f40e6d01484
anonymousrecords <a.Records2022@gmail.com>
2024-06-11 19:54:22 +0900

    feat : 로그인 x일 경우, 로그인 안내 창으로 보이게 하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
-import { AnimatedTitle } from "@/components/AnimatedTitle";
+import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import { AnimatedTitle } from '@/components/AnimatedTitle';
+import { options } from './api/auth/[...nextauth]/options';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    return (
+      <p className="text-center text-gray-700 mt-4">
+        로그인이 필요합니다.
+        <Link href="/auth" className="ml-2 text-[#FFBF00] hover:underline">
+          로그인 하기
+        </Link>
+      </p>
+    );
+  }
+
   return (
     <main>
       <div>홈 페이지입니다.</div>
