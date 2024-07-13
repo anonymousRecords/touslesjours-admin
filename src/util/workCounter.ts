665abc91d695193b4d4a5f514ac7e92250cec2b4
anonymousrecords <a.Records2022@gmail.com>
2024-07-13 16:59:18 +0200

    chore: mentionCounter 개선

--- src/util/workCounter.ts
+++ src/util/workCounter.ts
@@ -1,17 +1,16 @@
 import { AssignedWorkers } from '@/data/type';
 
 // 주말 배열에서 특정 담당자의 언급 횟수를 카운트하는 함수
-const mentionCounter = (personName: string, assignedWorkers: AssignedWorkers[]): number => {
-  let mentionCount = 0;
+const getMetionCount = (personName: string, assignedWorkers: AssignedWorkers[]): number => {
+  const mentionCount = assignedWorkers.reduce((acc, assignedWork) => {
+    if (assignedWork.workers == null) {
+      return acc;
+    }
 
-  // 주말 배열을 순회하면서 특정 담당자의 언급 횟수를 카운트
-  for (const assingedWorkds of assignedWorkers) {
-    const workers = assingedWorkds.workers;
-    // workers 배열에서 "일번사람"이 언급된 횟수를 카운트
-    mentionCount += workers.filter(worker => worker === personName).length;
-  }
+    return acc + assignedWork.workers.filter((worker) => worker === personName).length;
+  }, 0);
 
   return mentionCount;
 };
 
-export default mentionCounter;
+export default getMetionCount;
