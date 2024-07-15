dd8ce04846900a2c19c475550dea654700664213
anonymousrecords <a.Records2022@gmail.com>
2024-07-15 22:08:19 +0200

    chore: Avatar component 구현

--- src/app/cold-plate/page.tsx
+++ src/app/cold-plate/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { format } from 'date-fns';
+import { format, isWeekend } from 'date-fns';
 import { useEffect, useMemo, useState } from 'react';
 import Calendar from 'react-calendar';
 import { getWorkSchedules, insertWorkSchedule, upsertWorkSchedule } from '@/apis/workSchedule';
@@ -13,23 +13,23 @@ import { assignWorkers } from '@/util/assignedWorkers';
 import getMetionCount from '@/util/getMetionCount';
 import { getMonthlyWorkSchedules } from '@/util/getMonthlyWorkScheduleData';
 import { isPastDate } from '@/util/isPastDate';
-import { isWeekend } from '@/util/isWeekend';
 import { generateMonthlyWeekendArray } from '@/util/weekendArrayGenerator';
 
 import 'react-calendar/dist/Calendar.css';
+import Avatar from './components/Avatar/Avatar';
 
 const dropdownContents = ['1번 뚜둥이', '2번 뚜둥이', '3번 뚜둥이'];
 
 export default function Home() {
   const [selectedWorker1, setSelectedWorker1] = useState<string | null>(null);
   const [selectedWorker2, setSelectedWorker2] = useState<string | null>(null);
-  const [workScheduleData, setWorkScheduleData] = useState<WorkSchedule[]>([]);
+  const [workSchedulesData, setWorkSchedulesData] = useState<WorkSchedule[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   // 저장하기 버튼 클릭 시 실행되는 함수
   const handleSaveClick = async () => {
     // selectedDate로 선택한 날짜의 데이터를 가져와서 수정
-    const selectedData = workScheduleData.find((worker) => worker.date === selectedDate);
+    const selectedData = workSchedulesData.find((worker) => worker.date === selectedDate);
 
     if (selectedData && selectedWorker1 && selectedWorker2) {
       const newData = {
@@ -41,7 +41,7 @@ export default function Home() {
 
       const updatedData = await getWorkSchedules();
       const monthlyWorkScheduleData = getMonthlyWorkSchedules(updatedData, monthOfSelectedDate);
-      setWorkScheduleData(monthlyWorkScheduleData);
+      setWorkSchedulesData(monthlyWorkScheduleData);
     }
 
     setSelectedWorker1(null);
@@ -82,10 +82,10 @@ export default function Home() {
     // 주말이면서 현재나 과거 날짜가 아니면 상태 변경
     if (isWeekend(date) && !isPastDate(date)) {
       setSelectedWorker1(
-        workScheduleData.find((worker) => worker.date === selectedDate)?.workers?.[0] || null,
+        workSchedulesData.find((worker) => worker.date === selectedDate)?.workers?.[0] || null,
       );
       setSelectedWorker2(
-        workScheduleData.find((worker) => worker.date === selectedDate)?.workers?.[1] || null,
+        workSchedulesData.find((worker) => worker.date === selectedDate)?.workers?.[1] || null,
       );
       setIsModalOpen(true);
     }
@@ -113,7 +113,7 @@ export default function Home() {
 
       const monthlyWorkScheduleData = getMonthlyWorkSchedules(updatedData, monthOfSelectedDate);
 
-      setWorkScheduleData(monthlyWorkScheduleData);
+      setWorkSchedulesData(monthlyWorkScheduleData);
     };
 
     addWorkSchedule();
@@ -122,7 +122,7 @@ export default function Home() {
 
   // 작업자 목록 출력
   const renderWorkers = () => {
-    return workScheduleData.map((assignment) => {
+    return workSchedulesData.map((assignment) => {
       if (!assignment.workers) {
         return null;
       }
@@ -139,9 +139,9 @@ export default function Home() {
   };
 
   // 특정 담당자의 작업 횟수 카운트
-  const oneWorkermentionCounts = getMetionCount('1번 뚜둥이', workScheduleData);
-  const twoWorkermentionCounts = getMetionCount('2번 뚜둥이', workScheduleData);
-  const threeWorkermentionCounts = getMetionCount('3번 뚜둥이', workScheduleData);
+  const oneWorkermentionCounts = getMetionCount('1번 뚜둥이', workSchedulesData);
+  const twoWorkermentionCounts = getMetionCount('2번 뚜둥이', workSchedulesData);
+  const threeWorkermentionCounts = getMetionCount('3번 뚜둥이', workSchedulesData);
 
   return (
     <div className="flex flex-col bg-gray-100 min-h-screen ">
@@ -161,33 +161,50 @@ export default function Home() {
           }}
           showNeighboringMonth={false}
           tileContent={({ date }) => {
-            const parsedDate = format(date, 'yyyy-MM-dd');
-            for (let i = 0; i < workScheduleData.length; i++) {
-              if (parsedDate === workScheduleData[i].date) {
-                return (
-                  <div className="flex gap-1">
-                    <div
-                      className="rounded-full w-4 h-4"
-                      style={{
-                        backgroundColor:
-                          personList.find(
-                            (person) => person.name === workScheduleData[i].workers?.[0],
-                          )?.color || 'gray',
-                      }}
-                    ></div>
-                    <div
-                      className="rounded-full w-4 h-4"
-                      style={{
-                        backgroundColor:
-                          personList.find(
-                            (person) => person.name === workScheduleData[i].workers?.[1],
-                          )?.color || 'gray',
-                      }}
-                    ></div>
-                  </div>
-                );
-              }
+            if (!isWeekend(date)) {
+              return null;
             }
+
+            const workSchedule = workSchedulesData.find(
+              (worker) => worker.date === format(date, 'yyyy-MM-dd'),
+            );
+
+            const worker1 = personList.find((person) => person.name === workSchedule?.workers?.[0]);
+            const worker2 = personList.find((person) => person.name === workSchedule?.workers?.[1]);
+
+            return (
+              <div className="flex gap-1">
+                <Avatar backgroundColor={worker1?.color || 'lightgray'} />
+                <Avatar backgroundColor={worker2?.color || 'lightgray'} />
+              </div>
+            );
+
+            // for (let i = 0; i < workScheduleData.length; i++) {
+            //   if (parsedDate === workScheduleData[i].date) {
+            //     return (
+            //       <div className="flex gap-1">
+            //         <div
+            //           className="rounded-full w-4 h-4"
+            //           style={{
+            //             backgroundColor:
+            //               personList.find(
+            //                 (person) => person.name === workScheduleData[i].workers?.[0],
+            //               )?.color || 'gray',
+            //           }}
+            //         ></div>
+            //         <div
+            //           className="rounded-full w-4 h-4"
+            //           style={{
+            //             backgroundColor:
+            //               personList.find(
+            //                 (person) => person.name === workScheduleData[i].workers?.[1],
+            //               )?.color || 'gray',
+            //           }}
+            //         ></div>
+            //       </div>
+            //     );
+            //   }
+            // }
           }}
         />
         {isModalOpen && (
