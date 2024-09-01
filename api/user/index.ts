694abd5b791084b500c91da047a4714afc6eed14
anonymousrecords <a.Records2022@gmail.com>
2024-09-01 18:05:29 +0900

    feat: user api 구현

---
+++ api/user/index.ts
@@ -0,0 +1,16 @@
+'use server';
+import { User } from '../../types/types';
+import { createClient } from '../../utils/supabase/server';
+
+
+export const getUsers = async (): Promise<User[]> => {
+  const supabase = createClient();
+  const { data, error } = await supabase.from('users').select('id, username');
+
+  if (error) {
+    console.error('Error fetching users:', error);
+    return [];
+  }
+
+  return data || [];
+};
