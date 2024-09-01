8f50ce2b3ea69606497c42fea9a23240c0522b65
anonymousrecords <a.Records2022@gmail.com>
2024-09-01 18:05:18 +0900

    feat: sandwich api 구현

---
+++ api/sandwich/index.ts
@@ -0,0 +1,57 @@
+'use server';
+import { SandwichSchedule } from '../../types/types';
+import { createClient } from '../../utils/supabase/server';
+
+export const getSandwichSchedulesByPeriod = async (period: string): Promise<SandwichSchedule[]> => {
+  const supabase = createClient();
+  const { data, error } = await supabase.from('sandwich_schedule').select('*').eq('period', period);
+
+  if (error) {
+    console.error('Error fetching sandwich schedules:', error);
+    return [];
+  }
+
+  return data || [];
+};
+
+export const getSandwichScheduleByDate = async (date: string): Promise<SandwichSchedule[]> => {
+  const supabase = createClient();
+  const { data, error } = await supabase.from('sandwich_schedule').select('*').eq('date', date);
+
+  if (error) {
+    console.error('Error fetching sandwich schedule:', error);
+    return [];
+  }
+
+  return data || [];
+};
+
+export const upsertSandwichSchedule = async (
+  newSandwichSchedule: SandwichSchedule,
+): Promise<void> => {
+  const supabase = createClient();
+  const { date, days, sandwich_type: sandwichType } = newSandwichSchedule;
+
+  if (!date || !days || !sandwichType) {
+    console.error('Missing required fields for upsert');
+    return;
+  }
+
+  const { error } = await supabase.from('sandwich_schedule').upsert({
+    ...newSandwichSchedule,
+    id: `${date}${sandwichType}`,
+  });
+
+  if (error) {
+    console.error('Error upserting sandwich schedule:', error);
+  }
+};
+
+export const deleteSandwichSchedule = async (date: string): Promise<void> => {
+  const supabase = createClient();
+  const { error } = await supabase.from('sandwich_schedule').delete().eq('date', date);
+
+  if (error) {
+    console.error('Error deleting sandwich schedule:', error);
+  }
+};
